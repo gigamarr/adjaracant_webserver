2c8cd6bfa65f5d63c1ea50a6227e8690b50d6449
gigamarr <nofacemarr@gmail.com>
2020-08-05 21:47:56 +0400

    initial

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/search/<string:keywords>')
+def search(keywords):
+    response = requests.get(f"https://api.adjaranet.com/api/v1/search?[type]=movie&keywords={keywords}")
+    return response.json()
+
+@app.route('/get-data/<string:movie_id>')
+def get_data(movie_id):
+    response = requests.get(f"https://api.adjaranet.com/api/v1/movies/{movie_id}?source=adjaranet")
+    return response.json()
+
+@app.route('/get-files/<string:movie_id>/<string:season_index>')
+def get_movie_files(movie_id, season_index):
+    response = requests.get(f"https://api.adjaranet.com/api/v1/movies/{movie_id}/season-files/{season_index}?source=adjaranet")
+    return response.json()
