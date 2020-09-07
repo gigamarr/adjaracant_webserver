11a3821453ddfe31b29fc795c0aa708b27ea9df7
gigamarr <nofacemarr@gmail.com>
2020-09-07 15:16:29 +0400

    changed api url that was breaking seasons

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ def search(keywords):
 
 @app.route('/get-data/<string:movie_id>')
 def get_data(movie_id):
-    response = requests.get(f"https://api.adjaranet.com/api/v1/movies/{movie_id}?source=adjaranet")
+    response = requests.get(f"https://api.adjaranet.com/api/v1/movies/{movie_id}?filters[with_directors]=3&source=adjaranet")
     return response.json()
 
 @app.route('/get-files/<string:movie_id>/<string:season_index>')
 def get_movie_files(movie_id, season_index):
     response = requests.get(f"https://api.adjaranet.com/api/v1/movies/{movie_id}/season-files/{season_index}?source=adjaranet")
     return response.json()
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
